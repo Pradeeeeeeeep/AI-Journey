c59782982b248b942c80571d7b9df4f9416c2794
Pradeep <246137392+Pradeeeeeeeep@users.noreply.github.com>
2026-07-02 02:33:29 +0530

    basic commands

---
+++ Foundation/list.py
@@ -0,0 +1,11 @@
+my_list = [1, 2, 3, 4, 5]
+
+print("Original list:", my_list)
+print("Length of the list:", len(my_list))
+print("First element:", my_list[0])
+my_list.append(int(6))
+print("List after appending 6:", my_list)
+my_list.insert(0, 0)
+print("List after inserting 0 at index 0:", my_list)
+
+
