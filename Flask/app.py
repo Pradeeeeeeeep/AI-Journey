100f7bbd2d714f28e7067bebcc7ecb4b331a0bb8
Pradeep <246137392+Pradeeeeeeeep@users.noreply.github.com>
2026-07-02 09:22:01 +0530

    basic navebar with finction

--- Flask/app.py
+++ Flask/app.py
@@ -2,12 +2,32 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/<name>')
-def home(name):
-    return render_template('index.html', name = name)
+@app.route('/')
+@app.route('/home')
+def index():
+    return render_template('index.html')
+
+# @app.route('/<name>')
+# def home(name):
+#     return render_template('index.html', name = name)
+
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 @app.route('/dashboard/<name>/<age>/<city>/<course>')
 def dashboard(name, age, city, course):
-    return render_template('dashboard.html', name=name, age=age, city=city, course=course)P/
+    skills = ['Python', 'Flask', 'HTML', 'CSS', 'JavaScript']
+    eligible = course == 'AIML' and age >= '18'
+    return render_template('dashboard.html', name=name, age=age, city=city, course=course, skills=skills, eligible=eligible)
+
+
+
+@app.route('/youtube/<id>')
+def youtube(id):
+    return render_template('youtbePlayer.html', id=id)
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=True)
