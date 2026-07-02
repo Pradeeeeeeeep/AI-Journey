6b06bfeaf6c92742a036b08270a550f622437601
Pradeep <246137392+Pradeeeeeeeep@users.noreply.github.com>
2026-07-02 07:16:47 +0530

    dashboard UI flask

--- Flask/app.py
+++ Flask/app.py
@@ -2,10 +2,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('index.html', name = "LORD")
+@app.route('/<name>')
+def home(name):
+    return render_template('index.html', name = name)
 
-
+@app.route('/dashboard/<name>/<age>/<city>/<course>')
+def dashboard(name, age, city, course):
+    return render_template('dashboard.html', name=name, age=age, city=city, course=course)P/
 if __name__ == "__main__":
     app.run(debug=True)
