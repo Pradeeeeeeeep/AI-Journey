a501f3ef8d03573cecd658aeb6f091836bc90eb2
Pradeep <246137392+Pradeeeeeeeep@users.noreply.github.com>
2026-07-02 06:53:59 +0530

    adding pages using route

--- Flask/app.py
+++ Flask/app.py
@@ -6,5 +6,18 @@ app = Flask(__name__)
 def hello_world():
     return 'MY NAME is Pradeep!'
 
+
+@app.route("/about")
+def about():
+    return "I am a software engineer with a passion for developing innovative programs that expedite the efficiency and effectiveness of organizational success. I am skilled in technology and writing code to create systems that are reliable and user-friendly. I am also a quick learner and can adapt to new technologies as they emerge."
+
+
+
+@app.route('/count')
+def count():
+    my_list = ['apple', 'banana', 'cherry', 'date', 'elderberry']
+    return f'The number of items in the list is: {len(my_list)}'
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001, debug=True)
