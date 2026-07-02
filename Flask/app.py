b81f417cd9b59265d062eeaceaae5fd682b32f32
Pradeep <246137392+Pradeeeeeeeep@users.noreply.github.com>
2026-07-02 06:50:40 +0530

    first API return

--- Flask/app.py
+++ Flask/app.py
@@ -1,7 +1,10 @@
-form flask import Flask
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "Hello, World!"
+def hello_world():
+    return 'MY NAME is Pradeep!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001, debug=True)
