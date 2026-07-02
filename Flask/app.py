96858eca107d7c507168ec6f26532aa54a38f86e
Pradeep <246137392+Pradeeeeeeeep@users.noreply.github.com>
2026-07-02 16:33:10 +0530

    trying Login with Flask

--- Flask/app.py
+++ Flask/app.py
@@ -1,33 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/home')
+@app.route('/home', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
-
-# @app.route('/<name>')
-# def home(name):
-#     return render_template('index.html', name = name)
-
-
-
-@app.route("/about")
-def about():
-    return render_template("about.html")
-
-@app.route('/dashboard/<name>/<age>/<city>/<course>')
-def dashboard(name, age, city, course):
-    skills = ['Python', 'Flask', 'HTML', 'CSS', 'JavaScript']
-    eligible = course == 'AIML' and age >= '18'
-    return render_template('dashboard.html', name=name, age=age, city=city, course=course, skills=skills, eligible=eligible)
-
-
-
-@app.route('/youtube/<id>')
-def youtube(id):
-    return render_template('youtbePlayer.html', id=id)
+    if request.method == 'POST':
+        username = request.form['username']
+        return f"Hello, {username}!"
+    return render_template('login.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5001, debug=True)
