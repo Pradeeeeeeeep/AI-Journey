5690285e975bd774132ee06abdb6494c29fd6bed
Pradeep <246137392+Pradeeeeeeeep@users.noreply.github.com>
2026-07-07 01:09:50 +0530

    basic login logout page

--- Flask/app.py
+++ Flask/app.py
@@ -1,14 +1,38 @@
-from flask import Flask, render_template,request
+from flask import Flask, Response, request, redirect, url_for, session
 
 app = Flask(__name__)
+app.secret_key = "supersecretkey"
 
-@app.route('/')
-@app.route('/home', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         username = request.form['username']
-        return f"Hello, {username}!"
-    return render_template('login.html')
+        password = request.form['password']
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5001, debug=True)
+        if username == 'admin' and password == 'password':
+            session['user'] = username;
+            return redirect(url_for('success'))
+        else:
+            return Response('Invalid credentials', mimetype='text/plain')
+    return '''
+        <form method="post">
+            Username: <input type="text" name="username"><br>
+            Password: <input type="password" name="password"><br>
+            <input type="submit" value="Login">
+        </form>
+        '''
+
+@app.route('/success')
+def success():
+    if 'user' in session:
+        return f'''
+            <h1>Welcome, {session['user']}!</h1>
+            <a href="{url_for('logout')}">Logout</a>
+        '''
+    else:
+        return redirect(url_for('index'))
+    
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    return redirect(url_for('index'))
